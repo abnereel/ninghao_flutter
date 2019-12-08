35bdf404345046918995b8812511ed3f7aa46cd5
abnereel <408666378@qq.com>
2019-12-08 09:11:52 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ninghao_flutter/demo/basic_demo.dart';
 import './demo/listview_demo.dart';
 import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
-import './demo/listview_demo.dart';
+import './demo/layout_demo.dart';
+import './demo/view_demo.dart';
+import './demo/sliver_demo.dart';
+import './demo/navigator_demo.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -10,7 +14,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 去掉右上角的debug显示
-      home: Home(),
+      // home: NavigatorDemo(),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => Home(),
+        '/about': (context) => Page(title: 'About',)
+      },
       theme: ThemeData( // 设置主题
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
@@ -25,7 +34,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
@@ -47,14 +56,16 @@ class Home extends StatelessWidget {
               Tab(icon: Icon(Icons.local_florist)),
               Tab(icon: Icon(Icons.change_history)),
               Tab(icon: Icon(Icons.directions_bike)),
+              Tab(icon: Icon(Icons.view_quilt)),
             ],
           ),
         ),
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12,),
-            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12,),
+            BasicDemo(),
+            LayoutDemo(),
+            SliverDemo(),
           ],
         ),
         drawer: DrawerDemo(),
